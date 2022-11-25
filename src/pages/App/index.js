4de9220e060a0099e1f017dbc2cc721954728a65
Kevin Aya <kevinsaya25@gmail.com>
2022-11-24 23:33:40 -0500

    Fix height mobile

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,6 +1,7 @@
 import "./App.css";
 
 import React from "react";
+import Div100vh from "react-div-100vh";
 
 import { AppUI } from "./AppUI";
 
@@ -9,7 +10,9 @@ import { StateProvider } from "../../context/stateContext";
 const App = () => {
   return (
     <StateProvider>
-      <AppUI />
+      <Div100vh>
+        <AppUI />
+      </Div100vh>
     </StateProvider>
   );
 };
